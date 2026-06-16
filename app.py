8365ced272cb375dd936a0e037c316c3e266e133
kawtarherra212 <kawtarherra212@gmail.com>
2026-06-16 17:02:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,30 @@
-from flask import Flask, request, jsonify
-import tensorflow as tf
+import gradio as gr
 import numpy as np
+import tensorflow as tf
 import joblib
 
-app = Flask(__name__)
-
-# Load model and scaler
+# Load model + scaler
 model = tf.keras.models.load_model("final_model.keras")
 scaler = joblib.load("scaler.pkl")
 
-@app.route("/")
-def home():
-    return {"message": "Fraud Detection API is running"}
-
-@app.route("/predict", methods=["POST"])
-def predict():
-    data = request.json["features"]
-
-    x = np.array(data).reshape(1, -1)
-
+def predict(*features):
+    x = np.array(features).reshape(1, -1)
     x = scaler.transform(x)
 
-     # CNN-LSTM input format
+    # CNN-LSTM input format
     x = x.reshape(1, x.shape[1], 1)
 
     prob = float(model.predict(x, verbose=0)[0][0])
 
-    prediction = 1 if prob >= 0.5 else 0
+    prediction = "Fraud" if prob >= 0.5 else "Normal"
 
-    return jsonify({
-        "fraud_probability": prob,
-        "prediction": prediction
-    })
+    return prediction, prob
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+demo = gr.Interface(
+    fn=predict,
+    inputs=[gr.Number() for _ in range(29)],
+    outputs=["text", "number"],
+    title="Fraud Detection Model"
+)
+
+demo.launch(server_name="0.0.0.0", server_port=7860)
